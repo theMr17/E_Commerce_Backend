c11395c05fecd4c458c3177f7e7fbb69310963be
the-future-warrior <saptakmanna17@gmail.com>
2023-10-25 18:18:40 +0530

    Connects database

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const app = express();
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+dbConnect();
+
+app.use("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
+});
